56ed7862166703db37e25749be16d611148993fc
Komal Kumar <104495398+KK2082187@users.noreply.github.com>
2023-06-11 14:56:35 +0530

    Geekster SpringBoot

--- MailSendProject/src/main/java/org/geekster/Main.java
+++ MailSendProject/src/main/java/org/geekster/Main.java
@@ -3,7 +3,7 @@ package org.geekster;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("This application is supposed to send mails");
+        System.out.println("This application is supposed to send  mails");
 
         HandleMail mailer = new HandleMail();
         mailer.sendMail();
