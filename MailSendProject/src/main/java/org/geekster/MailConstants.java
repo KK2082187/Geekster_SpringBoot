d96308b2ed9a38437bffced3115647be3aafd343
Komal Kumar <104495398+KK2082187@users.noreply.github.com>
2023-06-11 14:56:00 +0530

    Geekster SpringBoot

--- MailSendProject/src/main/java/org/geekster/MailConstants.java
+++ MailSendProject/src/main/java/org/geekster/MailConstants.java
@@ -4,5 +4,5 @@ public class MailConstants {
     public static final String SENDER = "kk2082187@gmail.com";
     public static final String RECEIVER = "kk2082187@gmail.com";
     public static  final String SUBJECT = "Testing Java mail : 15-April-2023";
-    public static final String MESSAGE = "Hii Komal this message is for security check...!!!";
+    public static final String MESSAGE = "Hii Komal this message is for security check....!!!";
 }
