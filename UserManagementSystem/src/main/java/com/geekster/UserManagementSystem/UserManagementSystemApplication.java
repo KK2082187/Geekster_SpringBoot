ecdf574633256b6ce4aef29f74022b7e23d21dcf
Komal Kumar <104495398+KK2082187@users.noreply.github.com>
2023-06-22 11:48:41 +0530

    Geekster SpringBoot

--- UserManagementSystem/src/main/java/com/geekster/UserManagementSystem/UserManagementSystemApplication.java
+++ UserManagementSystem/src/main/java/com/geekster/UserManagementSystem/UserManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserManagementSystemApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementSystemApplication.class, args);
 	}
